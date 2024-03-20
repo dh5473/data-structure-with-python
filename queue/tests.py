f37fa213323362fe591f89247d6b978d74634c9d
dh5473 <dh5473@naver.com>
2024-03-20 23:17:51 +0900

    Feat: base test codes for queue

---
+++ queue/tests.py
@@ -0,0 +1,67 @@
+from queue_with_two_stacks import QueueWithStack
+from circular_queue import CircularQueue
+from linked_queue import LinkedQueue
+import unittest
+
+
+class LinkedQueueTest(unittest.TestCase):
+    def setUp(self):
+        self.queue = LinkedQueue()
+
+    def test_enqueue(self):
+        pass
+
+    def test_dequeue(self):
+        pass
+
+    def test_peek(self):
+        pass
+
+    def test_size(self):
+        pass
+
+    def test_is_empty(self):
+        pass
+
+
+class QueueWithStackTest(unittest.TestCase):
+    def setUp(self):
+        self.queue = QueueWithStack()
+    
+    def test_enqueue(self):
+        pass
+
+    def test_dequeue(self):
+        pass
+
+    def test_peek(self):
+        pass
+
+    def test_size(self):
+        pass
+
+    def test_is_empty(self):
+        pass
+
+
+class CircularQueueTest(unittest.TestCase):
+    def setUp(self):
+        self.queue = CircularQueue()
+
+    def test_enqueue(self):
+        pass
+
+    def test_dequeue(self):
+        pass
+
+    def test_front(self):
+        pass
+
+    def test_rear(self):
+        pass
+
+    def test_is_empty(self):
+        pass
+
+    def test_is_full(self):
+        pass
