f1be4b00d91ad96f0f7027f94c4c07a9a2c8895e
dh5473 <dh5473@naver.com>
2024-03-21 00:01:26 +0900

    Feat: test funcs for stack

--- stack/tests.py
+++ stack/tests.py
@@ -8,19 +8,65 @@ class LinkedStackTest(unittest.TestCase):
         self.stack = LinkedStack()
 
     def test_push(self):
-        pass
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+
+        self.assertEqual(self.stack.pop(), 3)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertEqual(self.stack.last, 1)
+
+        self.stack.push(4)
+        self.assertEqual(self.stack.pop(), 4)
+        self.assertEqual(self.stack.pop(), 1)
 
     def test_pop(self):
-        pass
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+
+        self.assertEqual(self.stack.pop(), 3)
+        self.assertEqual(self.stack.pop(), 2)
+
+        self.stack.push(4)
+        self.assertEqual(self.stack.pop(), 4)
+        self.assertEqual(self.stack.pop(), 1)
+
+        with self.assertRaises(IndexError):
+            self.stack.pop()
 
     def test_top(self):
-        pass
+        self.stack.push(1)
+        self.stack.push(2)
+
+        self.assertEqual(self.stack.top(), 2)
+        self.assertEqual(self.stack.top(), 2)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertEqual(self.stack.top(), 1)
+        self.stack.pop()
+
+        with self.assertRaises(IndexError):
+            self.stack.top()
 
     def test_size(self):
-        pass
+        self.stack.push(1)
+        self.assertEqual(self.stack.size(), 1)
+        
+        self.stack.push(2)
+        self.assertEqual(self.stack.size(), 2)
+
+        self.stack.pop()
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 0)
 
     def test_is_empty(self):
-        pass
+        self.assertEqual(self.stack.is_empty(), 1)
+
+        self.stack.push(1)
+        self.assertEqual(self.stack.is_empty(), 0)
+
+        self.stack.pop()
+        self.assertEqual(self.stack.is_empty(), 1)
 
 
 class StackWithQueueTest(unittest.TestCase):
@@ -28,16 +74,61 @@ class StackWithQueueTest(unittest.TestCase):
         self.stack = StackWithQueue()
 
     def test_push(self):
-        pass
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+
+        self.assertEqual(self.stack.pop(), 3)
+        self.assertEqual(self.stack.pop(), 2)
+
+        self.stack.push(4)
+        self.assertEqual(self.stack.pop(), 4)
+        self.assertEqual(self.stack.pop(), 1)
 
     def test_pop(self):
-        pass
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+
+        self.assertEqual(self.stack.pop(), 3)
+        self.assertEqual(self.stack.pop(), 2)
+
+        self.stack.push(4)
+        self.assertEqual(self.stack.pop(), 4)
+        self.assertEqual(self.stack.pop(), 1)
+
+        with self.assertRaises(IndexError):
+            self.stack.pop()
 
     def test_top(self):
-        pass
+        self.stack.push(1)
+        self.stack.push(2)
+
+        self.assertEqual(self.stack.top(), 2)
+        self.assertEqual(self.stack.top(), 2)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertEqual(self.stack.top(), 1)
+        self.stack.pop()
+
+        with self.assertRaises(ValueError):
+            self.stack.top()
 
     def test_size(self):
-        pass
+        self.stack.push(1)
+        self.assertEqual(self.stack.size(), 1)
+        
+        self.stack.push(2)
+        self.assertEqual(self.stack.size(), 2)
+
+        self.stack.pop()
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 0)
 
     def test_is_empty(self):
-        pass
+        self.assertEqual(self.stack.is_empty(), 1)
+
+        self.stack.push(1)
+        self.assertEqual(self.stack.is_empty(), 0)
+
+        self.stack.pop()
+        self.assertEqual(self.stack.is_empty(), 1)
