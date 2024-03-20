b8218dc7fb48aea76eab29f09f9e8562a15787ac
dh5473 <dh5473@naver.com>
2024-03-20 16:03:45 +0900

    Feat: base file for stack

---
+++ stack/stack_with_two_queues.py
@@ -0,0 +1,22 @@
+from collections import deque  # just data structure with doubly linked list
+# import queue -> for communication with different threads
+
+
+class StackWithQueue:
+    def __init__(self):
+        pass
+
+    def push(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def top(self):
+        pass
+
+    def size(self):
+        pass
+
+    def is_empty(self):
+        pass
