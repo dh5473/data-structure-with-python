9339dd2575a765a68495569e4d2874055c808f9c
dh5473 <dh5473@naver.com>
2024-03-20 23:17:33 +0900

    Feat: base test codes for stack

---
+++ stack/tests.py
@@ -0,0 +1,43 @@
+from stack_with_two_queues import StackWithQueue
+from linked_stack import LinkedStack
+import unittest
+
+
+class LinkedStackTest(unittest.TestCase):
+    def setUp(self):
+        self.stack = LinkedStack()
+
+    def test_push(self):
+        pass
+
+    def test_pop(self):
+        pass
+
+    def test_top(self):
+        pass
+
+    def test_size(self):
+        pass
+
+    def test_is_empty(self):
+        pass
+
+
+class StackWithQueueTest(unittest.TestCase):
+    def setUp(self):
+        self.stack = StackWithQueue()
+
+    def test_push(self):
+        pass
+
+    def test_pop(self):
+        pass
+
+    def test_top(self):
+        pass
+
+    def test_size(self):
+        pass
+
+    def test_is_empty(self):
+        pass
