db350d2b0dfa3d454e80e64e15a8e840f7f42c79
dh5473 <dh5473@naver.com>
2024-03-21 17:57:01 +0900

    Feat: base file for hash table

---
+++ hash_table/hash_table_with_double_hashing.py
@@ -0,0 +1,24 @@
+class HashTable:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def _hash1(self):
+        pass
+
+    def _hash2(self):
+        pass
+
+    def insert(self, key, value):
+        pass
+
+    def search(self, key):
+        pass
+
+    def remove(self, key):
+        pass
+
+    def rehash(self):
+        pass
