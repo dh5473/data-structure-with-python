44901fa9a6e81a7d1c31ff876aa88e6aabc5d6c0
dh5473 <dh5473@naver.com>
2024-03-22 00:39:24 +0900

    Feat: test codes for linked queue, queue with two stacks

--- 4_queue/tests.py
+++ 4_queue/tests.py
@@ -9,19 +9,67 @@ class LinkedQueueTest(unittest.TestCase):
         self.queue = LinkedQueue()
 
     def test_enqueue(self):
-        pass
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+        self.queue.enqueue(3)
+
+        self.assertEqual(self.queue.head.val, 1)
+        self.assertEqual(self.queue.tail.val, 3)
+        
+        self.queue.dequeue()
+        self.assertEqual(self.queue.head.val, 2)
+
+        self.queue.enqueue(4)
+        self.assertEqual(self.queue.tail.val, 4)
 
     def test_dequeue(self):
-        pass
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+        self.queue.enqueue(3)
+
+        self.assertEqual(self.queue.dequeue(), 1)
+        self.assertEqual(self.queue.dequeue(), 2)
+        self.assertEqual(self.queue.head.val, 3)
+
+        self.queue.dequeue()
+        with self.assertRaises(IndexError):
+            self.queue.dequeue()
 
     def test_peek(self):
-        pass
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+
+        self.assertEqual(self.queue.peek(), 1)
+        self.assertEqual(self.queue.peek(), 1)
+        
+        self.assertEqual(self.queue.dequeue(), 1)
+        self.assertEqual(self.queue.peek(), 2)
+
+        self.queue.dequeue()
+        with self.assertRaises(ValueError):
+            self.queue.peek()
 
     def test_size(self):
-        pass
+        self.assertEqual(self.queue.size(), 0)
+
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+        self.assertEqual(self.queue.size(), 2)
+        
+        self.queue.dequeue()
+        self.assertEqual(self.queue.size(), 1)
+
+        self.queue.dequeue()
+        self.assertEqual(self.queue.size(), 0)        
 
     def test_is_empty(self):
-        pass
+        self.assertEqual(self.queue.is_empty(), True)
+        
+        self.queue.enqueue(1)
+        self.assertEqual(self.queue.is_empty(), False)
+
+        self.queue.dequeue()
+        self.assertEqual(self.queue.is_empty(), True)
 
 
 class QueueWithStackTest(unittest.TestCase):
@@ -29,19 +77,76 @@ class QueueWithStackTest(unittest.TestCase):
         self.queue = QueueWithStack()
     
     def test_enqueue(self):
-        pass
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+        self.queue.enqueue(3)
+
+        self.assertListEqual(self.queue.stack_in, [1, 2, 3])
+
+        self.queue.dequeue()
+        self.assertListEqual(self.queue.stack_in, [])
+        self.assertListEqual(self.queue.stack_out, [3, 2])
+
+        self.queue.enqueue(4)
+        self.assertListEqual(self.queue.stack_in, [4])
+        self.assertListEqual(self.queue.stack_out, [3, 2])
 
     def test_dequeue(self):
-        pass
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+        self.queue.enqueue(3)
+
+        self.assertListEqual(self.queue.stack_in, [1, 2, 3])
+
+        self.assertEqual(self.queue.dequeue(), 1)
+        self.assertListEqual(self.queue.stack_in, [])
+        self.assertListEqual(self.queue.stack_out, [3, 2])
+
+        self.queue.enqueue(4)
+        self.assertListEqual(self.queue.stack_in, [4])
+        self.assertListEqual(self.queue.stack_out, [3, 2])
+        self.assertEqual(self.queue.dequeue(), 2)
+        self.assertEqual(self.queue.dequeue(), 3)
+        self.assertEqual(self.queue.dequeue(), 4)
+
+        with self.assertRaises(IndexError):
+            self.queue.dequeue()
 
     def test_peek(self):
-        pass
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+
+        self.assertEqual(self.queue.peek(), 1)
+        self.assertEqual(self.queue.peek(), 1)
+        
+        self.assertEqual(self.queue.dequeue(), 1)
+        self.assertEqual(self.queue.peek(), 2)
+
+        self.queue.dequeue()
+        with self.assertRaises(ValueError):
+            self.queue.peek()
 
     def test_size(self):
-        pass
+        self.assertEqual(self.queue.size(), 0)
+
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+        self.assertEqual(self.queue.size(), 2)
+        
+        self.queue.dequeue()
+        self.assertEqual(self.queue.size(), 1)
+
+        self.queue.dequeue()
+        self.assertEqual(self.queue.size(), 0)        
 
     def test_is_empty(self):
-        pass
+        self.assertEqual(self.queue.is_empty(), True)
+        
+        self.queue.enqueue(1)
+        self.assertEqual(self.queue.is_empty(), False)
+
+        self.queue.dequeue()
+        self.assertEqual(self.queue.is_empty(), True)
 
 
 class CircularQueueTest(unittest.TestCase):
@@ -65,3 +170,7 @@ class CircularQueueTest(unittest.TestCase):
 
     def test_is_full(self):
         pass
+
+
+if __name__ == '__main__':
+    unittest.main()
