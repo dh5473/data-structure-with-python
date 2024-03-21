c97f8e9a2733ffcb9856a381066a57a643af8988
dh5473 <dh5473@naver.com>
2024-03-22 00:38:14 +0900

    Feat: funcs for queue with two stacks

--- 4_queue/queue_with_two_stacks.py
+++ 4_queue/queue_with_two_stacks.py
@@ -1,18 +1,38 @@
 class QueueWithStack:
     def __init__(self):
-        pass
+        self.stack_in = []
+        self.stack_out = []
+        self.length = 0
 
-    def enqueue(self):
-        pass
+    def enqueue(self, item):
+        self.stack_in.append(item)
+        self.length += 1
 
     def dequeue(self):
-        pass
+        if not self.length:
+            raise IndexError("dequeue for empty queue")
+
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        
+        self.length -= 1
+        return self.stack_out.pop()
 
     def peek(self):
-        pass
+        if not self.length:
+            raise ValueError("peek for empty queue")
+
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        
+        return self.stack_out[-1]
 
     def size(self):
-        pass
+        return self.length
     
     def is_empty(self):
-        pass
+        if self.length:
+            return False
+        return True
