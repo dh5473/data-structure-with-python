ed4a8f7eb4fab28c6402cdde73bc8cdfa500033f
dh5473 <dh5473@naver.com>
2024-03-26 00:01:41 +0900

    Feat: test codes for hash table

--- 5_hash_table/tests.py
+++ 5_hash_table/tests.py
@@ -10,19 +10,121 @@ class SeparateChainingHashTableTest(unittest.TestCase):
         self.table = sc.HashTable()
 
     def test_insert(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 7)
 
     def test_search(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 7)
+
+        with self.assertRaises(KeyError):
+            self.table.search(5)
+
+        with self.assertRaises(KeyError):
+            self.table.search('1')
+        
+        with self.assertRaises(KeyError):
+            self.table.search('d')
 
     def test_remove(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.remove('a')
+        self.assertEqual(self.table.size, 2)
+        with self.assertRaises(KeyError):
+            self.table.search('a')
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 4)
+
+        self.table.remove(1)
+        self.assertEqual(self.table.size, 3)
+        with self.assertRaises(KeyError):
+            self.table.search(1)
+        
+        
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.remove((3, 4))
+        self.assertEqual(self.table.size, 4)
+        with self.assertRaises(KeyError):
+            self.table.search((3, 4))
 
     def test_rehash(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.size, 2)
+        self.assertEqual(self.table.capacity, 4)
 
-    def test_str(self):
-        pass
+        self.table.insert(1, 2)
+        self.table.insert(2, 3)
+        self.assertEqual(self.table.size, 4)
+        self.assertEqual(self.table.capacity, 8)
+
+        self.table.insert(3, 3)
+        self.table.insert(4, 4)
+        self.table.insert(5, 5)
+        self.assertEqual(self.table.size, 7)
+        self.assertEqual(self.table.capacity, 16)
 
 
 class LinearProbingHashTableTest(unittest.TestCase):
@@ -30,19 +132,121 @@ class LinearProbingHashTableTest(unittest.TestCase):
         self.table = lp.HashTable()
 
     def test_insert(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 7)
 
     def test_search(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 7)
+
+        with self.assertRaises(KeyError):
+            self.table.search(5)
+
+        with self.assertRaises(KeyError):
+            self.table.search('1')
+        
+        with self.assertRaises(KeyError):
+            self.table.search('d')
 
     def test_remove(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.remove('a')
+        self.assertEqual(self.table.size, 2)
+        with self.assertRaises(KeyError):
+            self.table.search('a')
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 4)
+
+        self.table.remove(1)
+        self.assertEqual(self.table.size, 3)
+        with self.assertRaises(KeyError):
+            self.table.search(1)
+        
+        
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.remove((3, 4))
+        self.assertEqual(self.table.size, 4)
+        with self.assertRaises(KeyError):
+            self.table.search((3, 4))
 
     def test_rehash(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.size, 2)
+        self.assertEqual(self.table.capacity, 4)
 
-    def test_str(self):
-        pass
+        self.table.insert(1, 2)
+        self.table.insert(2, 3)
+        self.assertEqual(self.table.size, 4)
+        self.assertEqual(self.table.capacity, 8)
+
+        self.table.insert(3, 3)
+        self.table.insert(4, 4)
+        self.table.insert(5, 5)
+        self.assertEqual(self.table.size, 7)
+        self.assertEqual(self.table.capacity, 16)
 
 
 class QuadraticProbingHashTableTest(unittest.TestCase):
@@ -50,19 +254,121 @@ class QuadraticProbingHashTableTest(unittest.TestCase):
         self.table = qp.HashTable()
 
     def test_insert(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 7)
 
     def test_search(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 7)
+
+        with self.assertRaises(KeyError):
+            self.table.search(5)
+
+        with self.assertRaises(KeyError):
+            self.table.search('1')
+        
+        with self.assertRaises(KeyError):
+            self.table.search('d')
 
     def test_remove(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.remove('a')
+        self.assertEqual(self.table.size, 2)
+        with self.assertRaises(KeyError):
+            self.table.search('a')
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 4)
+
+        self.table.remove(1)
+        self.assertEqual(self.table.size, 3)
+        with self.assertRaises(KeyError):
+            self.table.search(1)
+        
+        
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.remove((3, 4))
+        self.assertEqual(self.table.size, 4)
+        with self.assertRaises(KeyError):
+            self.table.search((3, 4))
 
     def test_rehash(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.size, 2)
+        self.assertEqual(self.table.capacity, 4)
 
-    def test_str(self):
-        pass
+        self.table.insert(1, 2)
+        self.table.insert(2, 3)
+        self.assertEqual(self.table.size, 4)
+        self.assertEqual(self.table.capacity, 8)
+
+        self.table.insert(3, 3)
+        self.table.insert(4, 4)
+        self.table.insert(5, 5)
+        self.assertEqual(self.table.size, 7)
+        self.assertEqual(self.table.capacity, 16)
 
 
 class DoubleHashingHashTableTest(unittest.TestCase):
@@ -70,19 +376,121 @@ class DoubleHashingHashTableTest(unittest.TestCase):
         self.table = dh.HashTable()
 
     def test_insert(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 7)
 
     def test_search(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 7)
+
+        with self.assertRaises(KeyError):
+            self.table.search(5)
+
+        with self.assertRaises(KeyError):
+            self.table.search('1')
+        
+        with self.assertRaises(KeyError):
+            self.table.search('d')
 
     def test_remove(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.search('a'), 1)
+        self.assertEqual(self.table.search('b'), 2)
+        self.assertEqual(self.table.size, 2)
+
+        self.table.insert('a', 3)
+        self.table.insert('c', 'cc')
+        self.assertEqual(self.table.search('a'), 3)
+        self.assertEqual(self.table.search('c'), 'cc')
+        self.assertEqual(self.table.size, 3)
+
+        self.table.remove('a')
+        self.assertEqual(self.table.size, 2)
+        with self.assertRaises(KeyError):
+            self.table.search('a')
+
+        self.table.insert(1, 3)
+        self.table.insert(2, 'a')
+        self.assertEqual(self.table.search(1), 3)
+        self.assertEqual(self.table.search(2), 'a')
+        self.assertEqual(self.table.size, 4)
+
+        self.table.remove(1)
+        self.assertEqual(self.table.size, 3)
+        with self.assertRaises(KeyError):
+            self.table.search(1)
+        
+        
+        self.table.insert((3, 4), 2)
+        self.table.insert(('a', 'b'), 'ab')
+        self.assertEqual(self.table.search((3, 4)), 2)
+        self.assertEqual(self.table.search(('a', 'b')), 'ab')
+        self.assertEqual(self.table.size, 5)
+
+        self.table.remove((3, 4))
+        self.assertEqual(self.table.size, 4)
+        with self.assertRaises(KeyError):
+            self.table.search((3, 4))
 
     def test_rehash(self):
-        pass
+        self.table.insert('a', 1)
+        self.table.insert('b', 2)
+        self.assertEqual(self.table.size, 2)
+        self.assertEqual(self.table.capacity, 4)
 
-    def test_str(self):
-        pass
+        self.table.insert(1, 2)
+        self.table.insert(2, 3)
+        self.assertEqual(self.table.size, 4)
+        self.assertEqual(self.table.capacity, 8)
+
+        self.table.insert(3, 3)
+        self.table.insert(4, 4)
+        self.table.insert(5, 5)
+        self.assertEqual(self.table.size, 7)
+        self.assertEqual(self.table.capacity, 16)
 
 
 if __name__ == '__main__':
