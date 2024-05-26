78a99799be5c89dd5bc041d0bb42f135f778e3ba
dh5473 <dh5473@naver.com>
2024-05-27 00:45:43 +0900

    Feat: base test codes for binary tree

---
+++ 7_binary_tree/tests.py
@@ -0,0 +1,58 @@
+import array_binary_tree
+import node_binary_tree
+import unittest
+
+
+class ArrayBinaryTreeTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_set_root(self):
+        pass
+
+    def test_set_left(self):
+        pass
+
+    def test_set_right(self):
+        pass
+
+    def test_find_parent(self):
+        pass
+    
+    def test_find_left_child(self):
+        pass
+
+    def test_find_right_child(self):
+        pass
+
+    def test_find_left_sibling(self):
+        pass
+    
+    def test_find_right_sibling(self):
+        pass
+
+
+class NodeBinaryTreeTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_set_root(self):
+        pass
+
+    def test_append_node(self):
+        pass
+
+    def test_delete_node(self):
+        pass
+
+    def test_delete_deepest_node(self):
+        pass
+
+    def test_preorder(self):
+        pass
+
+    def test_inorder(self):
+        pass
+
+    def test_postorder(self):
+        pass
